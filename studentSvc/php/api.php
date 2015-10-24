3b35ba0a1dff4848c9bb7e14758c7b9f0a43fec4
JFigue27 <jaimefigueroa84@gmail.com>
2015-10-23 22:41:25 -0600

    -Implemented two methods in API: 	-maestro_materias 	-alumnos_calficacion

--- studentSvc/php/api.php
+++ studentSvc/php/api.php
@@ -192,6 +192,102 @@
 			}
 		}
 
+		/*
+		 * Lista de materias por maestro.
+		 *
+		 */
+		private function maestro_materias() {
+			// Validate token
+			$jwt = $this->getJWT();
+			if($jwt!=""){
+				try{
+					$token = JWT::decode($jwt, $this->jwt_key, array('HS512'));
+					error_log(print_r('decoded token', TRUE));
+				}catch(Exception $e){
+					// The token could not be decoded.
+					// this is likely because the signature was not able to be verified.
+					error_log(print_r('Token signature not valid', TRUE));
+					$this->response('UNAUTHORIZED', 401);
+				}
+				// If succeed, query the database for available enrollments
+				// Obtener calificaciones de los usuarios
+				$query="select "
+				." metter_course.id_metter_course, cat_group.grade, cat_group.day_trip, cat_metter.metter_name, cat_teacher.name, cat_group.period, cat_group.year_of_course"
+				." from metter_course "
+				." inner join cat_group "
+				." on metter_course.id_group = cat_group.id_group"
+				." inner join cat_metter"
+				." on metter_course.id_metter = cat_metter.id_metter"
+				." inner join cat_teacher"
+				." on metter_course.id_teacher = cat_teacher.id_teacher"
+				." where cat_teacher.user_name = 'meliton.gonzalez' and cat_group.period = 'SEPTIEMBRE-DICIEMBRE'";
+				$r = $this->conn->query($query) or die($this->conn->error.__LINE__);
+				$rows = array();
+				// Obtener los datos de mysql y llenarlos en objeto de php
+				while($row = $r->fetch_assoc()){
+					$rows[]=array_map('utf8_encode', $row);
+				}
+
+				$jwt = $this->tokenize($token->data->userId, $token->data->userName);
+				$response_array = ['ErrorThrown'=>false, 'ResponseDescription'=>'Success','Result'=>$rows, 'Token'=>$jwt];
+				// Token renewal
+				$response = $this->json($response_array);
+				$this->response($response, 200);
+			}else{
+				// No token found in the header.
+				error_log(print_r('Token not found in request', TRUE));
+				$this->response('UNAUTHORIZED', 401);	
+			}
+		}
+
+		/*
+		 * Lista de alumnos para capturar calificaciones
+		 *
+		 */
+		private function alumnos_calificacion() {
+			// Validate token
+			$jwt = $this->getJWT();
+			if($jwt!=""){
+				try{
+					$token = JWT::decode($jwt, $this->jwt_key, array('HS512'));
+					error_log(print_r('decoded token', TRUE));
+				}catch(Exception $e){
+					// The token could not be decoded.
+					// this is likely because the signature was not able to be verified.
+					error_log(print_r('Token signature not valid', TRUE));
+					$this->response('UNAUTHORIZED', 401);
+				}
+				// If succeed, query the database for available enrollments
+				// Obtener calificaciones de los usuarios
+				$query= " select "
+				." student.id_student , metter_course.id_group , metter_course.id_metter_course , student.student_name, student.enroll_number, student.status "
+				." note.p1, note.p2 "
+				." from student "
+				." inner join metter_course "
+				." on student.id_group = metter_course.id_group "
+				." left join note "
+ 				." on note.id_metter_course = metter_course.id_metter_course "
+				." where metter_course.id_metter_course = " 261 
+				." order by student.student_name ";
+				$r = $this->conn->query($query) or die($this->conn->error.__LINE__);
+				$rows = array();
+				// Obtener los datos de mysql y llenarlos en objeto de php
+				while($row = $r->fetch_assoc()){
+					$rows[]=array_map('utf8_encode', $row);
+				}
+
+				$jwt = $this->tokenize($token->data->userId, $token->data->userName);
+				$response_array = ['ErrorThrown'=>false, 'ResponseDescription'=>'Success','Result'=>$rows, 'Token'=>$jwt];
+				// Token renewal
+				$response = $this->json($response_array);
+				$this->response($response, 200);
+			}else{
+				// No token found in the header.
+				error_log(print_r('Token not found in request', TRUE));
+				$this->response('UNAUTHORIZED', 401);	
+			}
+		}
+
 		private function tokenize($user_id, $user_name){
 			error_log(print_r('Tokenize for '.$user_id.': '.$user_name, TRUE));
 			// If user validation succeed, tokenize as in http://www.sitepoint.com/php-authorization-jwt-json-web-tokens/
