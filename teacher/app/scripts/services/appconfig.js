4cb2a23ec33b8aecc4938164a11de6bdbddf86a6
JFigue27 <jaimefigueroa84@gmail.com>
2015-11-13 12:21:56 -0700

    change de conection server on teacher app

--- teacher/app/scripts/services/appconfig.js
+++ teacher/app/scripts/services/appconfig.js
@@ -8,6 +8,6 @@
  * Constant in the teacherApp.
  */
 angular.module('teacherApp').constant('appConfig', {
-    //API_URL:'http://apps.capsonic.com/IQS_Backend/api/' 	//PRODUCTION
-    API_URL: 'https://centrouniversitariopasodelnorte.com/student_api/' //DEVELOPMENT
+    //API_URL:'https://centrouniversitariopasodelnorte.com/student_api/' 	//PRODUCTION
+    API_URL: 'http://localhost:9090/teacher_api/' //DEVELOPMENT
 });
